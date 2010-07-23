681f5de4ebf58eaee49c7f20bfba9034191e1ef9
Dennis Stam <dennis.stam@surf.nl>
2010-07-23 13:14:29 +0000

    debian_packages: 	- Added -y to say yes to all

--- debian_packages.sh
+++ debian_packages.sh
@@ -2,8 +2,8 @@
 
 echo "Simple bootstrap for debian\n\n"
 
-apt-get install flex rsync uuid-dev ncurses-dev libreadline-dev gperf \
+apt-get -y install flex rsync uuid-dev ncurses-dev libreadline-dev gperf \
 	pkg-config build-essential libglib2.0-dev libusb-dev gobject-introspection \
 	libblkid-dev libudev-dev libdevmapper-dev libssl-dev libacl1-dev
 
-apt-get build-dep udev
+apt-get -y build-dep udev
