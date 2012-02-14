85cf43bb95d6050415da428940c4f2479d8a7be3
Bas van der Vlies <bas.vandervlies@surfsara.nl>
2012-02-14 11:46:30 +0000

    first build the index files again and switched to aptitude

--- tools/debian_packages.sh
+++ tools/debian_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-PACKAGES="libusb-1.0-0-dev gperf pciutils libxml2-dev expat pkg-config libssl-dev libglib2.0-dev libacl1-dev gettext docbook-to-man flex libreadline5-dev uuid-dev libblkid-dev"
+PACKAGES="libusb-1.0-0-dev gperf pciutils libxml2-dev expat pkg-config libssl-dev libglib2.0-dev libacl1-dev gettext docbook-to-man flex libreadline-dev uuid-dev libblkid-dev"
 
 
-
-apt-get install --yes ${PACKAGES}
+aptitude update
+aptitude install ${PACKAGES}
