ad4cbefa5d108a09a6e9406a6c6a7f53160c312b
Bas van der Vlies <bas.vandervlies@surfsara.nl>
2012-04-18 13:09:39 +0000

    Added a lot of package require to build SALI for debian

--- tools/debian_packages.sh
+++ tools/debian_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES="libusb-1.0-0-dev gperf pciutils libxml2-dev expat pkg-config libssl-dev libglib2.0-dev libacl1-dev gettext docbook-to-man flex libreadline-dev uuid-dev libblkid-dev"
+PACKAGES="libusb-1.0-0-dev gperf pciutils libxml2-dev expat pkg-config libssl-dev libglib2.0-dev libacl1-dev gettext docbook-to-man flex libreadline-dev uuid-dev libblkid-dev libssl-dev zlib1g-dev gobject-introspection libreadline-dev libudev-dev libexpat1-dev"
 
 
 aptitude update
