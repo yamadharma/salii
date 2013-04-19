c2939bef1c2dcfb0ae358700f8683145cd8703dd
Dennis Stam <dennis.stam@surf.nl>
2013-04-19 09:02:16 +0000

    Updated kernel version for i386

--- tools/debian_packages.sh
+++ tools/debian_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES="make bison bzip2 libusb-1.0-0-dev gperf pciutils libxml2-dev expat pkg-config libssl-dev libglib2.0-dev libacl1-dev gettext docbook-to-man flex libreadline-dev uuid-dev libblkid-dev libssl-dev zlib1g-dev gobject-introspection libreadline-dev libudev-dev libexpat1-dev docbook-utils libxml2-dev linux-libc-dev libv4l-dev libusb-dev"
+PACKAGES="g++ gcc make bison bzip2 libusb-1.0-0-dev gperf pciutils libxml2-dev expat pkg-config libssl-dev libglib2.0-dev libacl1-dev gettext docbook-to-man flex libreadline-dev uuid-dev libblkid-dev libssl-dev zlib1g-dev gobject-introspection libreadline-dev libudev-dev libexpat1-dev docbook-utils libxml2-dev linux-libc-dev libv4l-dev libusb-dev libkmod-dev"
 
 aptitude update
 aptitude install ${PACKAGES}
