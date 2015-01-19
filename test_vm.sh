82a72d48d923acd913a7f6e4ed5917f1e8d7c9c9
Dennis Stam <dennis.stam@surf.nl>
2015-01-19 10:29:11 +0100

    Some major changes, dynamic initrd generation before starting vm

--- test_vm.sh
+++ test_vm.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 
+## Let's force their location, this makes this script a lot more simpler
+if [ ! -x "$(basename ${0})" ]
+then
+    echo "Please run this script only from the root project dir!"
+    exit 1
+fi
+
+## Some variables
 LATEST="ftp://ftp.surfsara.nl/pub/sali/sali-x86_64.tar.gz"
 EXTRACT_FILES="sali-1.6.3/x86_64/initrd.img sali-1.6.3/x86_64/kernel"
-BUILD_DIR=$(pwd)/build
-RSYNC_OPTS="-arv --exclude-from=$(pwd)/files/rsync_exclude"
+ROOT_DIR=$(pwd)
+BUILD_DIR=$ROOT_DIR/build
+RSYNC_OPTS="-arv --exclude-from=$ROOT_DIR/files/rsync_exclude"
 
-echo $RSYNC_OPTS
-exit 0
-
+## We need the following commands, just make sure your PATH is correct!
 CURL=$(which curl)
 TAR=$(which tar)
 QEMUIMG=$(which qemu-img)
 QEMUSYS=$(which qemu-system-x86_64)
 CPIO=$(which cpio)
 BUNZIP2=$(which bunzip2)
+RSYNC=$(which rsync)
 
+## Stop this script if we are missing a command, error message is a bit cryptic (will be resolved in the future)
 check_commands(){
     for cmd in $@
     do
         if [ ! -x "${cmd}" ]
         then
-            echo "Could not find command ${cmd} or is not executable"
+            echo "Could not find command '${cmd}' (or is empty) or is not executable"
             exit 1
         fi
     done
@@ -40,6 +49,7 @@ case "$(uname -s)" in
     ;;
 esac
 
+## Very simple argument parser
 case "${1}" in
     clean)
         echo "Removing $BUILD_DIR dir"
@@ -76,13 +86,38 @@ case "${1}" in
             $QEMUIMG create -f qcow2 $BUILD_DIR/test_disk.qcow2 100G >/dev/null 2>&1
         fi
 
-        $QEMUSYS -kernel $BUILD_DIR/kernel -initrd $BUILD_DIR/initrd.img -m 1024 -cpu Haswell -smp "cpus=2" -hda $BUILD_DIR/test_disk.qcow2 &
-        
+        ## Add initrd.img generator
+        if [ ! -e "$ROOT_DIR/build/initrd.img.out" ]
+        then
+            echo "Bunzipping initrd.img"
+            $BUNZIP2 -sk "$ROOT_DIR/build/initrd.img" >/dev/null 2>&1
+        fi
+
+        ## Make sure the old test_dir is gone
+        if [ -d "$ROOT_DIR/build/initrd_test" ]
+        then
+            echo "Removing previous initrd_test dir"
+            rm -rf "$ROOT_DIR/build/initrd_test"
+        fi
+
+        echo "Unpacking cpio image"
+        mkdir "$ROOT_DIR/build/initrd_test"
+        cd "$ROOT_DIR/build/initrd_test" && $CPIO -id < ../initrd.img.out >/dev/null 2>&1
+
+        echo "Rsyncing the test files"
+        $RSYNC $RSYNC_OPTS $ROOT_DIR/ $ROOT_DIR/build/initrd_test
+
+        echo "Creating new initrd"
+        cd "$ROOT_DIR/build/initrd_test" && find . | cpio --quiet -o -H newc > $ROOT_DIR/build/initrd_test.img.out
+
+        echo "Starting VM"
+        $QEMUSYS -kernel $BUILD_DIR/kernel -initrd $BUILD_DIR/initrd_test.img.out -m 1024 -cpu Haswell -smp "cpus=2" -hda $BUILD_DIR/test_disk.qcow2 &
+
+        echo "Just type ctr+c to exit"
+        wait
     ;;
     *)
         echo "Usage: ${0} <run|clean>"
     ;;
 esac
 
-echo "Test"
-wait
