87e649ba76f4aa094c3f26265196c50c467b3f0b
Dmitry S. Kulyabov <yamadharma@gmail.com>
2024-08-27 19:44:09 +0300

    fix(sali-cli): update information

--- sali/release.py
+++ sali/release.py
@@ -1,7 +1,7 @@
 #
-# This file is part of SALI
+# This file is part of SALII
 #
-# SALI is free software: you can redistribute it and/or modify
+# SALII is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
@@ -14,22 +14,22 @@
 # You should have received a copy of the GNU General Public License
 # along with SALI.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright 2010-2021 SURF
+# Copyright 2010-2024 Dmitry S. Kulyabov
 
 from sali.tools import version_info
 
-name            = 'sali'
+name            = 'salii'
 ## See also https://packaging.python.org/en/latest/distributing.html#version
-hexversion, version_info  = version_info(2,1,2,'',0)
+hexversion, version_info  = version_info(1,7,0,'',0)
 if version_info.releaselevel:
     version         = '%d.%d.%d%s' % (version_info.major, version_info.minor, version_info.micro, version_info.releaselevel)
 else:
     version         = '%d.%d.%d' % (version_info.major, version_info.minor, version_info.micro)
-copyright       = 'Copyright (c) 2010-2021 SURF'
-description     = 'SALI is used to image and install Linux machine with rsync or bittorrent, see https://gitlab.com/surfsara/sali for more information.'
-author          = 'SURF'
-author_email    = 'sali@surfsara.nl'
-url             = 'https://gitlab.com/surfsara/sali'
+copyright       = 'Copyright (c) 2010-2024 Dmitry S. Kulyabov'
+description     = 'SALII is used to image and install Linux machine with rsync or bittorrent.'
+author          = 'Dmitry S. Kulyabov'
+author_email    = 'yamadharma@gmail.com'
+url             = 'https://github.com/yamadharma/salii'
 license         = 'GPL'
 download_url    = '%s' % (version)
-keywords        = ['SURF', 'SystemImager', 'SALI', 'installer', 'tornando', 'bittorrent', 'linux']
+keywords        = ['SURF', 'SystemImager', 'SALII', 'installer', 'tornando', 'bittorrent', 'linux']
